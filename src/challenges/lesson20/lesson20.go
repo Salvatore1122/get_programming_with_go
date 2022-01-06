771fa3dbbde68247c595edffcb68f819d65c5b18
Your Name <kosuke.develop@gmail.com>
2022-01-07 01:02:00 +0900

    Lesson20を20.2まで回答

--- src/challenges/lesson20/lesson20.go
+++ src/challenges/lesson20/lesson20.go
@@ -1,6 +1,9 @@
 package lesson20
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 const (
 	width  = 80
@@ -22,6 +25,33 @@ func (u Universe) show() {
 	}
 }
 
+func (u Universe) seed() {
+	for i, row := range u {
+		for j, _ := range row {
+			u[i][j] = rand.Intn(2) == 1
+		}
+	}
+}
+
+func (u Universe) alive(x, y int) bool {
+	return u[(x+width)%(width-1)][(y+height)%(height-1)]
+}
+
+func (u Universe) neighbors(x, y int) int {
+	aliveCount := 0
+	for _, nums := range [][]int{{-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {1, 0}, {-1, 1}, {0, 1}, {1, 1}} {
+		if u.alive(x+nums[0], y+nums[1]) {
+			aliveCount++
+		}
+	}
+	return aliveCount
+}
+
+func (u Universe) next(x, y int) bool {
+	aliveCount := u.neighbors(x, y)
+	return aliveCount == 3 || (u.alive(x, y) && aliveCount == 2)
+}
+
 func newUniverse() Universe {
 	univ := make(Universe, height, height)
 	for i := 0; i < height; i++ {
@@ -31,5 +61,7 @@ func newUniverse() Universe {
 }
 
 func Answer() {
-	newUniverse().show()
+	univ := newUniverse()
+	univ.seed()
+	univ.show()
 }
